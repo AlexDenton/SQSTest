1a05f5bead620847792248f15ea8c62f31ec6a90
Alex Denton <alex.denton.dev@gmail.com>
2018-03-29 10:48:11 -0400

    Added outline for sending and receiving a message from SQS

--- sqstest/Program.cs
+++ sqstest/Program.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Amazon.SQS;
+using Amazon.SQS.Model;
 
 namespace SQSTest
 {
@@ -6,7 +10,21 @@ namespace SQSTest
     {
         static void Main(string[] args)
         {
+            Run().Wait();
+
             Console.WriteLine("Hello World!");
+        }
+
+        public static async Task Run()
+        {
+            var sqsClient = SQSHelper.GetSQSClient();
+
+            var sqsHelper = new SQSHelper();
+            var createQueueResponse = await sqsHelper.CreateSQSQueue();
+            var queueUrlResponse = await sqsHelper.GetQueueUrl();
+            var sendMessageResponse = await sqsHelper.SendSQSMessage(queueUrlResponse.QueueUrl);
+            var receiveMessageResponse = await sqsHelper.ReceiveSQSMessage(queueUrlResponse.QueueUrl);
+            await sqsHelper.ProcessReceiveMessageResponse(receiveMessageResponse);
         }
     }
 }
---
+++ sqstest/SQSHelper.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Amazon.SQS;
+using Amazon.SQS.Model;
+
+namespace SQSTest
+{
+    public class SQSHelper
+    {
+        private readonly AmazonSQSClient _AmazonSQSClient;
+
+        private const string _QueueName = "DentonQueue";
+
+        public SQSHelper()
+        {
+            _AmazonSQSClient = GetSQSClient();
+        }
+
+        public static AmazonSQSClient GetSQSClient()
+        {
+            var sqsConfig = new AmazonSQSConfig
+            {
+                ServiceURL = ""
+            };
+
+            return new AmazonSQSClient(sqsConfig);
+        }
+
+        public async Task<CreateQueueResponse> CreateSQSQueue()
+        {
+            var createQueueRequest = new CreateQueueRequest
+            {
+                QueueName = _QueueName,
+                Attributes = new Dictionary<string, string>
+                {
+                    {
+                        QueueAttributeName.VisibilityTimeout, "10"
+                    }
+                }
+            };
+
+            return await _AmazonSQSClient.CreateQueueAsync(createQueueRequest);
+        }
+
+        public async Task<GetQueueUrlResponse> GetQueueUrl()
+        {
+            var request = new GetQueueUrlRequest
+            {
+                QueueName = _QueueName,
+                QueueOwnerAWSAccountId = ""
+            };
+
+            return (await _AmazonSQSClient.GetQueueUrlAsync(request));
+        }
+
+        public async Task<SendMessageResponse> SendSQSMessage(string queueUrl)
+        {
+            var sendMessageRequest = new SendMessageRequest
+            {
+                QueueUrl = queueUrl,
+                MessageBody = "Hello there"
+            };
+
+            return await _AmazonSQSClient.SendMessageAsync(sendMessageRequest);
+        }
+
+        public async Task<ReceiveMessageResponse> ReceiveSQSMessage(string queueUrl)
+        {
+            var receiveMessageRequest = new ReceiveMessageRequest
+            {
+                QueueUrl = queueUrl
+            };
+
+            return await _AmazonSQSClient.ReceiveMessageAsync(receiveMessageRequest);
+        }
+
+        public async Task ProcessReceiveMessageResponse(ReceiveMessageResponse response)
+        {
+            foreach (var message in response.Messages)
+            {
+                Console.WriteLine(message.Body);
+            }
+        }
+    }
+}
